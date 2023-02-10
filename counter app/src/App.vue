688edc76e6e9feb4aeb8b799108174e3965b3201
Sardor-Abduhamidov007 <sardorabduhamidov007@gmail.com>
2023-02-10 20:51:11 +0500

    added counter app

---
+++ counter app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+   <h1>{{ count }}</h1>
+   <button v-on:click="subs">Subs</button>
+   <button v-on:click="add">Add</button>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.count += 1;
+    },
+    subs() {
+      this.count -= 1;
+    }
+  }
+} 
+</script>
+
